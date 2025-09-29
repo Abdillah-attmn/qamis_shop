83b13bfc39804fbd12c7ef1edd248ed2d00aae57
abdillah attoumani <a.attoumani13@outlook.fr>
2025-09-29 18:40:05 +0200

    Fix Bug and Remove search bar on navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :firstname, :lastname, :phone_number])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phone_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :firstname, :lastname, :phone_number])
   end
 end
