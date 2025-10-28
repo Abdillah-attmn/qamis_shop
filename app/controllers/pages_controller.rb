eaa8ba056804a0c92d20413f0ac8b073ac5540bb
abdillah attoumani <a.attoumani13@outlook.fr>
2025-10-28 20:05:53 +0100

    Change color website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @products = Product.all.limit(8)
+    @products = Product.all.limit(4)
   end
 end
